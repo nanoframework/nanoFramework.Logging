6b97487c52f7f4e28dd182a066581d53b8d0a6a7
Olivier Blaise <16148676+oblaise@users.noreply.github.com>
2022-01-25 15:58:27 +0100

    Syslog Logging Provider initial commit

---
+++ nanoFramework.Logging.Syslog/Facility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace nanoFramework.Logging.Syslog
+{
+	/// <summary>
+	/// Available facility types (RFC 3164).
+	/// </summary>
+	public enum Facility
+	{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+		Kern = 0,
+		User = 1,
+		Mail = 2,
+		Daemon = 3,
+		Auth = 4,
+		Syslog = 5,
+		LPR = 6,
+		News = 7,
+		UUCP = 8,
+		Cron = 9,
+		AuthPriv = 10,
+		FTP = 11,
+		NTP = 12,
+		Audit = 13,
+		Audit2 = 14,
+		Cron2 = 15,
+		Local0 = 16,
+		Local1 = 17,
+		Local2 = 18,
+		Local3 = 19,
+		Local4 = 20,
+		Local5 = 21,
+		Local6 = 22,
+		Local7 = 23
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+	}
+}
---
+++ nanoFramework.Logging.Syslog/SyslogLogger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace nanoFramework.Logging.Syslog
+{
+    /// <summary>
+    /// ILogger implmentation that send message log to a Syslog server following RFC3164 convention.
+    /// While not mandatory it is recommended to use <see cref="SyslogLoggerFactory"/> to create the SyslogLogger instances.
+    /// </summary>
+    public class SyslogLogger : ILogger
+    {
+        private SyslogClient _client;
+
+        /// <summary>
+        /// Create new logger based on existing SyslogClient. Recommended to use LoggerFactory patern trough SyslogLoggerFactory instead of direct use.
+        /// </summary>
+        /// <param name="client">SyslogClient to use for sending message</param>
+        /// <param name="categoryName">CategoryName of this logger used as TAG in the Syslog messages</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public SyslogLogger(SyslogClient client, string categoryName)
+        {
+            if (client is null)
+                throw new ArgumentNullException(nameof(client));
+            
+            _client = client;
+            LoggerCategoryName = categoryName;
+        }
+
+        /// <summary>
+        /// Minimum log level used by this logger.
+        /// </summary>
+        public LogLevel MinLogLevel { get; set; } //by default 0 -> Trace
+        
+        /// <summary>
+        /// CategoryName of this logger used as TAG in the Syslog messages.
+        /// </summary>
+        public string LoggerCategoryName { get; }
+
+        /// <summary>
+        /// Underlying Syslog client used by this logger.
+        /// </summary>
+        public SyslogClient Client => _client;
+
+        /// <inheritdoc />
+        public bool IsEnabled(LogLevel logLevel) => logLevel >= MinLogLevel;
+
+        /// <inheritdoc />
+        public void Log(LogLevel logLevel, EventId eventId, string state, Exception exception, MethodInfo format)
+        {
+            if (logLevel >= MinLogLevel && logLevel!=LogLevel.None)
+            {
+                string message;
+                if (format is null)
+                    message = exception == null ? $"{state}" : $"{state} {exception}";
+                else
+                    message = $"{(string)format.Invoke(null, new object[] { LoggerCategoryName, logLevel, eventId, state, exception })}";
+
+                _client.SendMessage(LogLevelToSeverity(logLevel), LoggerCategoryName, message);
+            }
+        }
+        
+        private Severity LogLevelToSeverity(LogLevel level) => level switch
+            {
+                LogLevel.Trace => Severity.Debug,
+                LogLevel.Debug => Severity.Debug,
+                LogLevel.Information => Severity.Informational,
+                LogLevel.Warning => Severity.Warning,
+                LogLevel.Error => Severity.Error,
+                LogLevel.Critical => Severity.Critical,
+                LogLevel.None => Severity.None,
+                _ => Severity.None
+            };
+
+    }
+}
