1e034868ca7763ad31c868c61b40eb9b1b2aaa47
DaveSchmid <code@wirzi.com>
2023-01-03 11:41:17 +0100

    - Unittests for LoggerExtensions.cs - {0}{1} formatted string as logmessage - json formatted string as logmessage
    Signed-off-by: DaveSchmid <code@wirzi.com>

--- Tests/UnitTestDebugLogging/LoggerExtensionsTests.cs
+++ Tests/UnitTestDebugLogging/LoggerExtensionsTests.cs
@@ -8,60 +8,101 @@ using nanoFramework.Logging;
 using nanoFramework.Logging.Debug;
 using nanoFramework.TestFramework;
 using System;
+using System.Diagnostics;
 
 namespace UnitTestDebugLogging
 {
     [TestClass]
     public class LoggerExtensionsTests
     {
-        public LoggerExtensionsTests Instance { get; set; }
-        private ILogger _logger;
-        private string _jsonString = @"{ ""Message"":""Huhu"" }";
-
-        [Setup]
-        public void SetupDebugTest()
+        public ILogger GetLogger()
         {
-            Instance = new LoggerExtensionsTests();
-            LogDispatcher.LoggerFactory = new DebugLoggerFactory();
-            Instance._logger = this.GetCurrentClassLogger();
+            var logger = new DebugLogger("LoggerExtensionsTests");
+            logger.MinLogLevel = LogLevel.Trace;
+            return logger;
         }
-
+        /// <summary>
+        /// LogInformation Test with formatted string with some parameters
+        /// </summary>
         [TestMethod]
-        public void LogTest()
+        public void LogInformationFormatedStringWithParams()
         {
-            //LogFormatedStringWithParams();
-            //LogJsonStringWitoutParams();
-            //Assert.ThrowsException(typeof(NullReferenceException), LogJsonStringWithParams);
+            GetLogger().LogInformation("{0}{1}{2}", "nano", "frame", "work");
+        }
+        /// <summary>
+        /// LogWarning Test with formatted string with some parameters
+        /// </summary>
+        [TestMethod]
+        public void LogWarningFormatedStringWithParams()
+        {
+            GetLogger().LogWarning("{0}{1}{2}", "nano", "frame", "work");
+        }
+        /// <summary>
+        /// LogTrace Test with formatted string with some parameters
+        /// </summary>
+        [TestMethod]
+        public void LogTraceFormatedStringWithParams()
+        {
+            GetLogger().LogTrace("{0}{1}{2}", "nano", "frame", "work");
+        }
+        /// <summary>
+        /// LogError Test with formatted string with some parameters
+        /// </summary>
+        [TestMethod]
+        public void LogErrorFormatedStringWithParams()
+        {
+            GetLogger().LogError("{0}{1}{2}", "nano", "frame", "work");
+        }
+        /// <summary>
+        /// LogDebug Test with formatted string with some parameters
+        /// </summary>
+        [TestMethod]
+        public void LogDebugFormatedStringWithParams()
+        {
+            GetLogger().LogDebug("{0}{1}{2}", "nano", "frame", "work");
+        }
+        /// <summary>
+        /// LogCritical Test with formatted string with some parameters
+        /// </summary>
+        [TestMethod]
+        public void LogCriticalFormatedStringWithParams()
+        {
+            GetLogger().LogCritical("{0}{1}{2}", "nano", "frame", "work");
+        }
+        /// <summary>
+        /// Log Test with formatted string with some parameters and all log levels
+        /// </summary>
+        [TestMethod]
+        public void LogFormatedStringWithParams()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                GetLogger().Log((LogLevel)i, "{0}{1}{2}", "nano", "frame", "work");
+            }
+        }
+        /// <summary>
+        /// string.Format on a json string doesn't work well
+        /// </summary>
+        [TestMethod]
+        public void LogJsonStringWithParams()
+        {
+            Assert.ThrowsException(typeof(ArgumentException), LogJsonStringWithParamsThrowEx);
+        }
+        /// <summary>
+        /// Incorrect call
+        /// </summary>
+        private void LogJsonStringWithParamsThrowEx()
+        {
+            GetLogger().LogError(@"{ ""Message"":""nanoframework"" }", 1, 2);
+        }
+        /// <summary>
+        /// Logging a json formatted string
+        /// </summary>
+        [TestMethod]
+        public void LogJsonStringWitoutParams()
+        {
+            GetLogger().LogInformation(@"{ ""Message"":""nanoframework"" }");
         }
 
-        private void LogFormatedStringWithParams()
-        {
-            //DebugLogger _logger;
-            //_logger = new DebugLogger("test");
-
-            //var log = LogDispatcher.GetLogger("Testlogger");
-            _logger.LogInformation("{1}{2}", 1, 2);
-        }
-        private void LogJsonStringWithParams()
-        {
-            //DebugLogger _logger;
-            //_logger = new DebugLogger("test");
-            //var log = LogDispatcher.GetLogger("Testlogger");
-            _logger.LogError(_jsonString, 1, 2);
-        }
-        private void LogJsonStringWitoutParams()
-        {
-            //DebugLogger _logger;
-            //_logger = new DebugLogger("test");
-
-            //var log = LogDispatcher.GetLogger("Testlogger");
-            _logger.LogInformation(_jsonString);
-        }
-
-        [Cleanup]
-        public void CleanDebugTest()
-        {
-            LogDispatcher.LoggerFactory = null;
-        }
     }
 }
